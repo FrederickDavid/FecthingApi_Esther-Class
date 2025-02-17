46de73a6b8ceee7c9e302bbb28fd53e9f1a8b2b8
FrederickDavid <muomaifefrederick61@gmail.com>
2025-02-17 14:39:35 +0100

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,82 @@
-import { useEffect, useState } from "react";
-import ProductCard from "./components/ProductCard";
-import axios from "axios";
+import styled from "@emotion/styled";
 
-function App() {
-  const [productData, setProductData] = useState();
-
-  const getAllProducts = async () => {
-    try {
-      const response = await axios.get("https://fakestoreapi.com/products");
-      console.log(response);
-      setProductData(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getAllProducts();
-  }, []);
+function ProductCard({ image, title, description, category, price }) {
   return (
-    <>
-      {productData?.map((props, index) => (
-        <ProductCard key={index} {...props} />
-      ))}
-    </>
+    <Container>
+      <Card>
+        <Image src={image} />
+        <Title>{title}</Title>
+        <Description>{description}</Description>
+        <Category>{category}</Category>
+        <Price>${price}</Price>
+      </Card>
+    </Container>
   );
 }
 
-export default App;
+export default ProductCard;
+
+const Price = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+  &:hover {
+    color: rgb(143, 31, 31);
+  }
+`;
+
+const Category = styled.div`
+  font-size: 9px;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  color: rgb(207, 117, 117);
+`;
+
+const Description = styled.div`
+  font-size: 12px;
+  text-align: center;
+  color: #6b6b6b;
+  margin: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 15px;
+  font-weight: 600;
+  text-align: center;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  &:hover {
+    color: rgb(143, 31, 31);
+  }
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  transition: 0.4s;
+  :hover {
+    transform: scale(1.3);
+  }
+`;
+
+const Card = styled.div`
+  width: 300px;
+  height: 350px;
+  margin: 30px;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0px;
+  align-items: center;
+  box-shadow: rgba(161, 56, 56, 0.16) 0px 1px 4px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  font-family: poppins;
+`;
